e4a68e1161a470d55db39c492189af313daad769
João <joao.costa7@estudante.ifms.edu.br>
2025-09-17 12:32:02 -0300

    criação de um post de produto

--- src/index-mongo.ts
+++ src/index-mongo.ts
@@ -1,6 +1,7 @@
 import 'dotenv/config';
 import express, { Request, Response } from 'express';
 import { MongoClient } from 'mongodb';
+import { stringify } from 'querystring';
 
 const client = new MongoClient(process.env.MONGO_URI!);
 const db = client.db(process.env.MONGO_DB!);
@@ -16,7 +17,17 @@ app.get('/produtos', async (req: Request, res: Response) => {
     res.send(produtos);
 
 });
-
+app.post('/produtos', async (req: Request, res: Response) => {
+    const {nome, preco,urlfoto, descricao,} = req.body;
+    if (!nome || !preco || !urlfoto || !descricao) {
+    //return res.status(400).send("nome, preco, urlfoto e descricao sao obrigatorios")
+    throw new Error("nome, preco, urlfoto e descricao sao obrigatorios")
+    }
+    const produto = {nome, preco,urlfoto, descricao,}
+    const resultado = await db.collection('produtos').insertOne(produto)
+    res.status(201).json({"nome,preco,urlfoto,descricao,_id:resultado.insertedId"});
+       
+    });
 app.listen(8000, () => {
     console.log('Server Iniciado, porta: 8000')
 });
